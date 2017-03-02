3429d18f27d595cd328af27c68a0c52e3755c808
Miwa Tritt <tmt7rc@virginia.edu>
2017-03-02 10:19:59 -0800

    Promises

--- src/ts/car-tool-app2/components/filtered-car-table.component.ts
+++ src/ts/car-tool-app2/components/filtered-car-table.component.ts
@@ -23,13 +23,25 @@ export class FilteredCarTable {
 
     private filterCache: Map<string, Car[]> = new Map<string, Car[]>();
 
+    public lastCars: Car[] = null;
+
     public get filteredCars () {
+        if(this.lastCars == null) {
+            this.lastCars = this.cars;
+        }
+
+        if(this.lastCars !== this.cars) {
+            this.filterCache.clear();
+            this.lastCars = this.cars;
+        }
+
         if(!this.filterCache.has(this.carMakeFilter)){
             //actually filter colors and store in map
             //pass callback function in filter
             this.filterCache.set(this.carMakeFilter, 
-                this.cars.filter((car) =>  car.make.toUpperCase().startsWith(this.carMakeFilter.toUpperCase())));
+                this.cars.filter((car) =>  car.make.toUpperCase().startsWith(this.carMakeFilter.toUpperCase()) ));
         }
+
         return this.filterCache.get(this.carMakeFilter);
     }
 }

6e050600b7211eeeb781ab818eeda08112ec20d3
Miwa Tritt <tmt7rc@virginia.edu>
2017-03-03 08:07:30 -0800

    Angular Forms

--- src/ts/car-tool-app2/app.component.ts
+++ src/ts/car-tool-app2/app.component.ts
@@ -6,13 +6,33 @@ import { Car } from "./interfaces/car";
     selector: "main",
     template: `
     <form novalidate>
-
+        <div>
+            <label for="first-name-input">First Name:</label>
+            <input type="text" id="first-name-input" 
+                name="firstNameInput" [(ngModel)]="firstName" 
+                required #firstNameInputRef="ngModel">
+            <span *ngIf="firstNameInputRef.invalid && firstNameInputRef.touched">
+                First Name is required
+            </span>
+            <br>Value: {{firstName}}, 
+                {{firstNameInputRef.invalid ? 'invalid' : 'valid'}}
+        </div>
+        <div>
+            <label for="last-name-input">Last Name:</label>
+            <input type="text" id="last-name-input" 
+                name="lastNameInput" [(ngModel)]="lastName" 
+                required>
+            <br>Value: {{lastName}}
+        </div>
     </form>
-
     `,
+    styles: [
+        "input.ng-invalid.ng-touched { border: red 1px solid; }",
+    ],
 })
 export class AppComponent { 
 
-    public message = "Hello World";
+    public firstName: string = "";
+    public lastName: string = "";
     
 }
